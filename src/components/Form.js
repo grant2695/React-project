d8a2f398a05da50178dddcfe0653336cb31b3a5e
grant2695 <grant.w.tucker@gmail.com>
2022-02-19 19:00:52 -0500

    made adjustments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,67 +1,104 @@
 
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
+
 import Todo from "./todo";
 
-
-
-
-const Form = ({setInputText, todos, setTodos,text, inputText,todo}) => {
-    const inputTextHandler = (e) =>{
-        console.log(e.target.value);
-        setInputText(e.target.value)
+const Form = ({
+  setInputText,
+  setStatus,
+  todos,
+  setTodos,
+  text,
+  inputText,
+  todo,
+  status,
+  filteredTodos,
+  setFilteredTodos,
+}) => {
+  const inputTextHandler = (e) => {
+    //console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+   const submitTodoHandler = (e) => {
        
+     setTodos([
+       ...todos,
+       { text: inputText, completed: false, id: Math.random() * 1000 },
+       
+     ]);
+     setInputText.reset();
+     e.preventDefault();
+   };
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+  
+  
+  return (
+    <>
+      <div className=" w-screen flex justify-start bg-black p-4">
+        <div className="text-white p-2 hover:bg-blue-400">About</div>
+        <div className="text-white p-2 hover:bg-blue-400">Contact</div>
+        <div className="text-white p-2 hover:bg-blue-400">Tasks</div>
+      </div>
 
-    }
-
-    const submitTodoHandler = (e) => {
-        setTodos([...todos, {text: inputText, completed: false, id: Math.random()*1000 }]     
-        
-        )
-           }
-        
-    return(
-        <>
-        <div className=' w-screen flex justify-start bg-black p-4'>
-     <div className='text-white p-2 hover:bg-blue-400'>About</div>
-     <div className='text-white p-2 hover:bg-blue-400'>Contact</div>
-     <div className='text-white p-2 hover:bg-blue-400'>Tasks</div>
-   </div>
-   
-       <div className={"flex justify-center mt-20"}>
-
-      <div className={'border w-1/2'}>
-          <div className={'flex gap-3 justify-center p-4'}>
-      <input onChange={inputTextHandler} type={'text'} className={'border w-2/3 px-1 outline-none rounded'} placeholder={'Task...'}/>
-     <button onClick={submitTodoHandler} className={" border p-1  w-1/3 border-blue-400 rounded"} type={'submit'}>Add</button>
-     
-</div>
-
-<div className={'flex justify-between px-4 bg-blue-100 p-2 border-b'}>
-                            <div>
-                                
-                                <span className={'ml-2 text-sm font-bold'}>Todo</span>
-                            </div>
-                            <div>
-                              <ul className="ml-3"><li>done/delete</li></ul>
-                            </div>
-
-            
-                        </div>                           
-                        {todos.map((todo)=>(
-        <Todo  todos = {todos} setTodos = {setTodos} key={todo.id}  todo={todo} text={todo.text}/>
-            
-           
-         
-    
-      
-     
-   
-    ))}
-
-    
-</div>
-</div>
+      <div className={"flex justify-center mt-20"}>
+        <div className={"border w-1/2"}>
+          <div className={"flex gap-3 justify-center p-4"}>
+            <form>
+              <input
+                onChange={inputTextHandler}
+                type={"text"}
+                className={"border w-2/3 px-1 outline-none rounded"}
+                placeholder={"Task..."}
+              />
+              <button
+                onClick={submitTodoHandler}
+                className={" border p-1  w-1/4  rounded"}
+                type={"submit"}
+              >
+                <i class="fas fa-plus-square"></i>
+              </button>
+            </form>
+          </div>
+          <form>
+            <div className="d-flex">
+              <div className="select text-center p-2">
+                <select
+                  onChange={statusHandler}
+                  name="todos"
+                  className=" border filter-todo border-blue-400 rounded"
+                >
+                  <option value="all">All</option>
+                  <option value="completed">Completed</option>
+                  <option value="uncompleted">Uncompleted</option>
+                </select>
+              </div>
+            </div>
+          </form>
+          <div className={"flex justify-between px-4 bg-blue-100 p-2 border-b"}>
+            <div>
+              <span className={"ml-2 text-sm font-bold"}>Todo</span>
+            </div>
+            <div>
+              <ul className="ml-3">
+                <li>done/delete</li>
+              </ul>
+            </div>
+          </div>
+          {filteredTodos.map((todo) => (
+            <Todo
+              todos={todos}
+              setTodos={setTodos}
+              key={todo.id}
+              todo={todo}
+              text={todo.text}
+            />
+          ))}
+        </div>
+      </div>
     </>
-    )}
+  );
+};
 
 export default Form;
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,40 +1,43 @@
 import React from "react";
 
-const Todo = ({text, todos, setTodos , todo}) =>{
-    const deleteHandler = ()=>{
-        setTodos(todos.filter((el)=> el.id !== todo.id ));
-        
-    }
-    const completeHandler = () => {
-        setTodos(todos.map(function(item){
-            if(item.id === todo.id){
-                return{
-                    ...item, completed: !item.completed,
-                }
-            }
-            return item;
-        }))
-    }
-          
-    return(
+const Todo = ({ text, todos, setTodos, todo,filteredTodos }) => {
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+  const completeHandler = () => {
+    setTodos(
+      todos.map(function (item) {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
 
+  return (
+    <div>
+      <div className={"flex justify-between px-4 mt-1 bg-gray-100"}>
         <div>
-        <div className={'flex justify-between px-4 mt-1 bg-gray-100'}>
-<div>
-  <span className={`ml-2 text-sm font-semibold ${todo.completed ? "completed" : " "}`}>{text}</span>
-</div>
-<div>
-<button onClick={completeHandler}><i className="fa-solid fa-circle-check text-blue-500 p-1"></i></button>
-    <button onClick={deleteHandler} ><i className="fa-solid fa-trash text-sm text-red-500 p-1"></i></button>
-   
-
-                
-    
-    </div>
+          <span
+            className={`ml-2 text-sm font-semibold      
+            ${todo.completed ? "completed" : " "}`}>
+            {text}
+          </span>
         </div>
-            </div>
-     
-    )
-    
-}
+        <div>
+          <button onClick={completeHandler}>
+            <i className="fa-solid fa-circle-check text-blue-500 p-1"></i>
+          </button>
+          <button onClick={deleteHandler}>
+            <i className="fa-solid fa-trash text-sm text-red-500 p-1"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Todo;
